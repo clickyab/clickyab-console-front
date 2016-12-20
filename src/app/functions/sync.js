d7f21c9dfa2ad09999c4a3422bfe6e8a1311ee27
hassan gilak <hasan.gilak@outlook.com>
2016-12-20 05:14:15 +0330

    middleware is finished

--- src/app/functions/sync.js
+++ src/app/functions/sync.js
@@ -8,40 +8,44 @@ export function sync(generator, main, ho) {
 		iterator = generator;
 	}
 
-	result = iterator.next();
-
-	if (result.value.next) {
-		result = result.value.next().value;
-	}
-
-	if (result.value) {
-		if (result.value.then)
-			result = result.value;
-	}
-
-	if (result.then) {
-		result.then((response) => {
-			result = iterator.next(response);
-			if (result.done) {
-				if (main) {
-					result = main.next(response);
-					if(!result.done) {
-						sync(result.value, main)
-					}
-				}
-
-			} else {
-				sync(result.value, iterator);
-			}
-		});
+	if (iterator.then) {
+		iterator.then((response) => main.next(response))
 	} else {
+		result = iterator.next();
+
+		if (result.value.next) {
+			result = result.value.next().value;
+		}
+
 		if (result.value) {
-			result = main.next(result.value);
-			if (!result.done) {
-				sync(result.value, main, true);
-			}
+			if (result.value.then)
+				result = result.value;
+		}
+
+		if (result.then) {
+			result.then((response) => {
+				result = iterator.next(response);
+				if (result.done) {
+					if (main) {
+						result = main.next(response);
+						if (!result.done) {
+							sync(result.value, main)
+						}
+					}
+
+				} else {
+					sync(result.value, iterator);
+				}
+			});
 		} else {
-			iterator.next(result);
+			if (result.value) {
+				result = main.next(result.value);
+				if (!result.done) {
+					sync(result.value, main, true);
+				}
+			} else {
+				iterator.next(result);
+			}
 		}
 	}
 }
--- src/app/middlewares/routes/onUserEnterMiddleware.js
+++ src/app/middlewares/routes/onUserEnterMiddleware.js
@@ -23,18 +23,22 @@ function* ff() {
 	yield 'chejoori ha ha ha ha';
 }
 export default (nextState, replace, next) => sync(function*() {
-	let login = yield isLoginMiddleware();
-	console.log('we are here', login);
-	let hasan = yield checkUserRoutePermission();
-	console.log('answer', hasan);
-	let a = yield aa();
-	console.log('a', a);
-	let b = yield bb();
-	console.log('b', b);
-	let c= yield cc();
-	console.log('c', c);
-	let f= yield ff();
-	console.log('f', f);
+	// let login = yield isLoginMiddleware();
+	// console.log('we are here', login);
+	// let hasan = yield checkUserRoutePermission();
+	// console.log('answer', hasan);
+	// let a = yield aa();
+	// console.log('a', a);
+	// let b = yield bb();
+	// console.log('b', b);
+	// let c= yield cc();
+	// console.log('c', c);
+	// let f= yield ff();
+	// console.log('f', f);
+	let pingo = yield ping();
+	console.log(pingo);
+	let bbb = yield ping();
+	console.log(bbb);
 	// runMiddleware([isLoginMiddleware]);
 	next()
 });
