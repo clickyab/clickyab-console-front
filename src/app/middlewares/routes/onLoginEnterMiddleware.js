e470cc7f69f336d476c38d241865352aa1577532
hassan gilak <hasan.gilak@outlook.com>
2016-12-20 06:20:33 +0330

    login middleware updated

--- src/app/middlewares/routes/onLoginEnterMiddleware.js
+++ src/app/middlewares/routes/onLoginEnterMiddleware.js
@@ -1,20 +1,10 @@
-import swagger from '../../swagger/index';
-import {getToken, isLogin} from "../../redux/helpers";
-import {sync} from '../../functions/sync';
-
-function redirectIfLogined(replace) {
-
-}
-
-
-
+import {sync} from "../../functions/sync";
+import ping from "../../functions/ping";
 
 export default (nextState, replace, next) => sync(function*() {
-    if (isLogin()) {
-        let user = yield ping();
-        console.log('milad milad miald dorosdt ');
-        next();
-    } else {
-        next();
-    }
+	let pingo = yield ping();
+	console.log(pingo);
+
+	next()
 });
+
