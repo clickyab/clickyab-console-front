aa00c06987f87f0e10d7979eaa26967cec3ef46f
hasan gilak <hasan.gilak@outlook.com>
2016-12-24 15:15:54 +0330

    user

--- src/app/redux/reducers/userReducer.js
+++ src/app/redux/reducers/userReducer.js
@@ -2,7 +2,58 @@ import {UPDATE_USER} from '../actions/user';
 
 
 const initial = {
-  token: '',
+    "corporation": {
+        "address": "string",
+        "city_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "country_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "created_at": "string",
+        "economic_code": "string",
+        "phone": "string",
+        "province_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "register_code": "string",
+        "title": "string",
+        "updated_at": "string",
+        "user_id": 0
+    },
+    "email": "string",
+    "perm": {},
+    "personal": {
+        "address": "string",
+        "birthday": "string",
+        "cellphone": "string",
+        "city_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "country_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "created_at": "string",
+        "first_name": "string",
+        "gender": "string",
+        "last_name": "string",
+        "national_code": "string",
+        "phone": "string",
+        "province_id": {
+            "Int64": 0,
+            "Valid": true
+        },
+        "updated_at": "string",
+        "user_id": 0,
+        "zip_code": "string"
+    },
+    "token": "string",
+    "user_id": 0
 };
 export default function userReducer(state = initial, action) {
     switch (action.type) {
