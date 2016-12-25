6b93f1e61d1de45650fb0eb47ffbf502c210a015
miladbruce <miladheydari.work@gmail.com>
2016-12-25 10:37:34 +0330

    set height

--- src/app/routes.js
+++ src/app/routes.js
@@ -44,84 +44,18 @@ export default () => (
 );
 
 history.listen(location => {
-    function getResponsiveBreakpoint(size) {
-        // bootstrap responsive breakpoints
-        let sizes = {
-            'xs': 480,     // extra small
-            'sm': 768,     // small
-            'md': 992,     // medium
-            'lg': 1200     // large
+    $(document).ready(function() {
+        function setHeight() {
+            let windowHeight = $(window).innerHeight();
+            $('.page-content').css({
+                "min-height": windowHeight+"px"
+            });
         };
-
-        return sizes[size] ? sizes[size] : 0;
-    }
-
-    let resBreakpointMd = getResponsiveBreakpoint('md');
-
-    function getViewPort() {
-        let e = window,
-            a = 'inner';
-        if (!('innerWidth' in window)) {
-            a = 'client';
-            e = document.documentElement || document.body;
-        }
-
-        return {
-            width: e[a + 'Width'],
-            height: e[a + 'Height']
-        };
-    }
-
-    function _calculateFixedSidebarViewportHeight() {
-        let sidebarHeight = getViewPort().height - $('.page-header').outerHeight(true);
-        if ($('body').hasClass("page-footer-fixed")) {
-            sidebarHeight = sidebarHeight - $('.page-footer').outerHeight();
-        }
-
-        return sidebarHeight;
-    };
-
-    function setSize() {
-        let content = $('.page-content');
-        let sidebar = $('.page-sidebar');
-        let body = $('body');
-        let height;
-
-        if (body.hasClass("page-footer-fixed") === true && body.hasClass("page-sidebar-fixed") === false) {
-            let available_height = getViewPort().height - $('.page-footer').outerHeight() - $('.page-header').outerHeight();
-            let sidebar_height = sidebar.outerHeight();
-            if (sidebar_height > available_height) {
-                available_height = sidebar_height + $('.page-footer').outerHeight();
-            }
-            if (content.height() < available_height) {
-                content.css('min-height', available_height);
-            }
-        } else {
-            if (body.hasClass('page-sidebar-fixed')) {
-                height = _calculateFixedSidebarViewportHeight();
-                if (body.hasClass('page-footer-fixed') === false) {
-                    height = height - $('.page-footer').outerHeight();
-                }
-            } else {
-                let headerHeight = $('.page-header').outerHeight();
-                let footerHeight = $('.page-footer').outerHeight();
-
-                if (getViewPort().width < resBreakpointMd) {
-                    height = getViewPort().height - headerHeight - footerHeight;
-                } else {
-                    height = sidebar.height() + 20;
-                }
-
-                if ((height + headerHeight + footerHeight) <= getViewPort().height) {
-                    height = getViewPort().height - headerHeight - footerHeight;
-                }
-            }
-            content.css('min-height', height);
-        }
-    }
-
-    $(document).ready(setSize());
-    $(window).resize(setSize());
+        setHeight();
+        $(window).resize(function() {
+            setHeight();
+        });
+    });
 });
 
 
