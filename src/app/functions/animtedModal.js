5e9be2d83cc6d8f6c23b41da1d03b24d73abb215
kasra <kasra.ansari@yahoo.com>
2016-12-25 14:26:25 +0330

    add animated modal

--- src/app/functions/animtedModal.js
+++ src/app/functions/animtedModal.js
@@ -1,5 +1,8 @@
 
-export function fullWidthModal(id) {
+export function fullWidthModal(id, title) {
     console.log('open modal');
-    $('#' + id).animatedModal()
+    $('#' + id).animatedModal();
+    let modalId =  $('#'+id).attr('href');
+    console.log(modalId);
+    $(modalId).find('.modal-title h3').text(title);
 }
