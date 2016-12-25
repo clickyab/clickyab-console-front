af95513775f5659df3c6ed02c4c54f9c478239bb
miladbruce <miladheydari.work@gmail.com>
2016-12-25 15:12:32 +0330

    modal form

--- src/app/functions/animtedModal.js
+++ src/app/functions/animtedModal.js
@@ -1,8 +1,6 @@
 
 export function fullWidthModal(id, title) {
-    $('#' + id).on('click', function() {
         $('#' + id).animatedModal();
         let modalId =  $('#'+id).attr('href');
         $(modalId).find('.modal-title h3').text(title);
-    })
 }
