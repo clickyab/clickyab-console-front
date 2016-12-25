72b2cdca79dbb1d97170216f453df7efcd90fdf1
kasra <kasra.ansari@yahoo.com>
2016-12-25 14:40:50 +0330

    fix some bug

--- src/app/functions/animtedModal.js
+++ src/app/functions/animtedModal.js
@@ -1,8 +1,8 @@
 
 export function fullWidthModal(id, title) {
-    console.log('open modal');
-    $('#' + id).animatedModal();
-    let modalId =  $('#'+id).attr('href');
-    console.log(modalId);
-    $(modalId).find('.modal-title h3').text(title);
+    $('#' + id).on('click', function() {
+        $('#' + id).animatedModal();
+        let modalId =  $('#'+id).attr('href');
+        $(modalId).find('.modal-title h3').text(title);
+    })
 }
