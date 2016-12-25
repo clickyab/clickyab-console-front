bdd44695413371656016b900a70d10e8ae78ae0a
hassan gilak <hasan.gilak@outlook.com>
2016-12-25 11:30:22 +0330

    select

--- src/app/swaggerPromiseBind/ping.js
+++ src/app/swaggerPromiseBind/ping.js
@@ -1,7 +1,5 @@
 import swagger from '../swagger/index';
-import {getToken} from "../redux/helpers";
-import {asyncUpdateLocalStorage} from "../functions/asyncUpdateLocalStorage";
-
+import select from './../functions/select-data';
 export default function ping() {
-    return new swagger.UserApi().userPingGet(getToken());
+    return new swagger.UserApi().userPingGet(select('user.token', 'no token'));
 }
