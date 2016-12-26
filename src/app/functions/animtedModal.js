4d08fc15daf4a072acf53050912b70733fe0f0b6
kasra <kasra.ansari@yahoo.com>
2016-12-26 16:25:56 +0330

    animated modal add option

--- src/app/functions/animtedModal.js
+++ src/app/functions/animtedModal.js
@@ -1,7 +1,8 @@
 
-export function fullWidthModal(id, title) {
+export function fullWidthModal(id, title, options = {}) {
         $('#' + id).animatedModal({
                 modalTarget:id+'-modal',
+            ...options
         });
         let modalId =  $('#'+id).attr('href');
         $(modalId).find('.modal-title h3').text(title);
