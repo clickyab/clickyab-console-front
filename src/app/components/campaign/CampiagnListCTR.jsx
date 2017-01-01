643d6059191f6984bcb5524f8ea093f890f04a7b
hasan gilak <hasan.gilak@outlook.com>
2017-01-01 10:26:43 +0330

    datatable

--- src/app/components/campaign/CampiagnListCTR.jsx
+++ src/app/components/campaign/CampiagnListCTR.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import CampaignListPTR from './CampaignListPTR';
+import Edit from './../common/Edit';
 
 @connect(({campaignList}) => ({campaignList}))
 export default class CampaignListCTR extends Component {
@@ -16,10 +17,16 @@ export default class CampaignListCTR extends Component {
         console.log(event.target.value, query_name)
     }
 
+    edit(id) {
+        return <Edit key={Math.random()} id={id}/>
+    }
+
+
     render() {
         const {items, definitions} = this.props.campaignList;
 
         return (<CampaignListPTR items={items} definitions={definitions}
+                                 edit={this.edit.bind(this)}
                                  sort={this.sort.bind(this)}
                                  filter={this.filter.bind(this)}
                                  search={this.search.bind(this)}
