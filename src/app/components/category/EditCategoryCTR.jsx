867d75a79e754dc6646799c663f47a4851dcd610
kasra <kasra.ansari@yahoo.com>
2017-01-01 16:14:46 +0330

    update edit category code

--- src/app/components/category/EditCategoryCTR.jsx
+++ src/app/components/category/EditCategoryCTR.jsx
@@ -1,24 +1,46 @@
 import React, {Component} from 'react';
 import EditCategoryPTR from './EditCategoryPTR';
 import swagger from './../../swagger/index';
+import {FailedBoxAlert} from "./../../functions/notifications";
+import {ifInvalidToken} from "./../../functions/helpers";
+import {sync} from "./../../functions/sync";
+import {select} from "./../../functions/select";
 import {connect} from 'react-redux';
+let Ladda = require('ladda/js/ladda');
+let loadingProgress;
 
 @connect()
-class EditCategoryCTR extends Component {
-    SubmitCall = (values, form)=> {
-        values.scope = 'channel';
-        (new swagger.CategoryApi())
-            .categoryEditIdPut('11','1:7c47910224365dd5f48e6a48d1348836f8c1550c', {
-                'payloadData': values
-            }, function(error, data, response) {
-                console.log(error, data, response)
-            })
+export default class EditCategoryCTR extends Component {
 
+    EditSubmit(formValues) {
+        const id = '11'; //TODO: replace with props
+        formValues.scope = 'channel';
+        sync(function *() {
+            loadingProgress = Ladda.create(document.querySelector('.edit-category-form'));
+            loadingProgress.start();
+            const {error, data, response} = yield (new swagger.CategoryApi())
+                .categoryEditIdPut(id, select('user.token', 'no token'), {'payloadData': formValues});
+
+            if (response.statusCode == '200') {
+                $('#editCategoryModal').modal('hide');
+                loadingProgress.stop();
+                //TODO: dispatch(data)
+            } else if (response.statusCode == '400') {
+                FailedBoxAlert(response)
+            }
+            ifInvalidToken(response);
+        })
+    }
+
+    SubmitEditCategory = (formValues, form)=> {
+       if(!form.valid)
+           return;
+
+        this.EditSubmit(formValues);
     };
 
     render() {
-        return (<EditCategoryPTR SubmitCall={this.SubmitCall}/>);
+        const {form} = this.props; //TODO: category data must be add
+        return (<EditCategoryPTR form={form} SubmitEditCategory={this.SubmitEditCategory}/>);
     }
 }
-
-export default EditCategoryCTR;
