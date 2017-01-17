5dbcf1a2ce1e723862afc66d6fba1b1291db7a1c
kasra <kasra.ansari@yahoo.com>
2017-01-17 11:53:02 +0330

    check if step one was done navigate to step two

--- src/app/middlewares/routes/onCreateCampaignStepOneOnEnterMiddleware.js
+++ src/app/middlewares/routes/onCreateCampaignStepOneOnEnterMiddleware.js
@@ -2,14 +2,19 @@ import {sync} from '../../functions/sync';
 import {loading} from '../../functions/loading';
 import {isLoginMiddleware} from "../isLoginMiddleware";
 import {handleError} from "../../functions/catchError";
+import {select} from './../../functions/select';
+import {navigate} from "../../functions/navigate";
 
 export default (nextState, replace, next) => sync(function*() {
     try {
         loading(true);
 
         yield* isLoginMiddleware();
-
-        next();
+        if (select('createCampaignData.id') != null) {
+            navigate('/v1/campaign/create/step/2');
+        } else {
+            next();
+        }
     } catch (error) {
         handleError(error);
     }
