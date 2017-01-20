e6b226cd445bd81f7addd13f7faa035e1bf552b1
kasra <kasra.ansari@yahoo.com>
2017-01-20 12:48:30 +0330

    change select to Field

--- src/app/components/location/SelectLocationPTR.jsx
+++ src/app/components/location/SelectLocationPTR.jsx
@@ -3,7 +3,7 @@ import $ from "jquery";
 import {Field, reduxForm} from "redux-form";
 import {select} from "../../functions/select";
 
-class SelectLocationPTR extends Component {
+export default class SelectLocationPTR extends Component {
 
     // handleInitialize() {
     //     const initData = select("user.personal", {}, true);
@@ -21,30 +21,30 @@ class SelectLocationPTR extends Component {
                 <div className="col-md-4">
                     <div className="form-group">
                         <label>انتخاب کشور</label>
-                        <select className="form-control select-country" id="country_id" data-placeholder="انتخاب کشور" tabIndex="1"
-                                name="country_id">
+                        <Field component='select' className="form-control select-country" id="country_id" data-placeholder="انتخاب کشور" tabIndex="1"
+                               name="country_id">
                             <option>انتخاب کشور</option>
-                        </select>
+                        </Field>
                     </div>
                 </div>
 
                 <div className="col-md-4">
                     <div className="form-group">
                         <label>استان</label>
-                        <select className="form-control select-province" data-placeholder="انتخاب استان" tabIndex="1"
-                                name="province_id">
+                        <Field component="select" className="form-control select-province" data-placeholder="انتخاب استان" tabIndex="1"
+                               name="province_id">
                             <option>انتخاب استان</option>
 
-                        </select>
+                        </Field>
                     </div>
                 </div>
                 <div className="col-md-4">
                     <div className="form-group">
                         <label>شهر</label>
-                        <select className="form-control select-city" data-placeholder="انتخاب شهر" tabIndex="1"
-                                name="city_id">
+                        <Field component="select" className="form-control select-city" data-placeholder="انتخاب شهر" tabIndex="1"
+                               name="city_id">
                             <option>انتخاب شهر</option>
-                        </select>
+                        </Field>
                     </div>
                 </div>
             </div>
@@ -53,6 +53,3 @@ class SelectLocationPTR extends Component {
 
 }
 
-export default reduxForm({
-    form: 'SelectLocationPTR'
-})(SelectLocationPTR);
