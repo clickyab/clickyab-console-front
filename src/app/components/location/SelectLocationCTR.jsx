832e2053baa093f9d9b4a0869760d70b8bea5274
kasra <kasra.ansari@yahoo.com>
2017-01-22 15:19:05 +0330

    location update

--- src/app/components/location/SelectLocationCTR.jsx
+++ src/app/components/location/SelectLocationCTR.jsx
@@ -1,98 +1,82 @@
-import React, {Component} from 'react';
-import SelectLocationPTR from './SelectLocationPTR';
-import swagger from './../../swagger/index';
+import React, {Component} from "react";
+import SelectLocationPTR from "./SelectLocationPTR";
+import swagger from "./../../swagger/index";
 import {getToken} from "../../redux/helpers";
 import {ifInvalidToken} from "../../functions/helpers";
 import {sync} from "../../functions/sync";
-import {select} from "../../functions/select";
+import {getCity} from "../../redux/helpers";
+import {getProvince} from "../../redux/helpers";
 
 export default class SelectLocationCTR extends Component {
-    callProvinceName(id) {
-        sync(function* (){
-            let {data, response, error } = yield (new swagger.LocationApi())
-                .locationProvinceGetIdGet(id, select('user.token', 'no token'))
 
-            if(response.statusCode == '200') {
-                $(".select-province").html('<option value=' + data.id + '>' + data.name + '</option>')
-                    .prop('disabled', true);
-            }
-        })
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            countries: [],
+            provinces: [],
+            cities: []
+        };
     }
 
-    callCityName(id) {
-        sync(function* (){
-            let {data, response, error } = yield (new swagger.LocationApi())
-                .locationCityGetIdGet(id, select('user.token', 'no token'))
+    requestToGetCountry() {
+        sync(function* () {
+            let {data, response} = yield (new swagger.LocationApi())
+                .locationCountryGet(getToken());
 
-            if(response.statusCode == '200') {
-                $(".select-city").html('<option value=' + data.id + '>' + data.name + '</option>')
-                    .prop('disabled', true);
+            if (response.statusCode == 200) {
+                this.setState({countries: data})
             }
-        })
+
+            ifInvalidToken(response);
+        }.bind(this));
+
+        this.getProvince();
     }
+
+    getCities(event) {
+        let id = event.target.value;
+        sync(function* () {
+            let {data, response} = yield (new swagger.LocationApi())
+                .locationCityIdGet(id, getToken());
+            if (response.statusCode == 200) {
+                this.setState({cities: data});
+            }
+
+            ifInvalidToken(response);
+        }.bind(this))
+    }
+
+    getProvince() {
+        sync(function*() {
+            let {data, response} = yield (new swagger.LocationApi())
+                .locationProvinceGet(getToken());
+
+            if (response.statusCode == 200) {
+                this.setState({provinces: data});
+            }
+
+            ifInvalidToken(response);
+        }.bind(this));
+    }
+
+    onCountryChanged(event) {
+        this.getProvince();
+    }
+
+    onProvinceChanged(event) {
+        this.getCities(event);
+    }
+
     componentDidMount() {
-        function getCountryList({error, data, response}) {
-            if (response.statusCode == 200) {
-                response.body.forEach( function( item )
-                {
-                    $(".select-country").append('<option value=' + item.id + '>' + item.name + '</option>');
-                });
-            } else if (response.statusCode == 400) {
-
-            }
-             ifInvalidToken(response);
-            }
-
-
-        function getProvinceList({error, data, response}) {
-            if (response.statusCode == 200) {
-                response.body.forEach( function( item )
-                {
-                    $(".select-province").append('<option value=' + item.id + '>' + item.name + '</option>');
-                });
-            } else if (response.statusCode == 400) {
-            }
-            ifInvalidToken(response)
+        this.requestToGetCountry();
+        if(getCity() != 0) {
+            this.getCities({target:{value:getProvince()}});
         }
-        function getCityList({error, data, response}) {
-            if (response.statusCode == 200) {
-                response.body.forEach( function( item )
-                {
-                    $(".select-city").append('<option value=' + item.id + '>' + item.name + '</option>');
-                });
-            } else if (response.statusCode == 400) {
-            }
-            ifInvalidToken(response)
-        }
-
-        function requestToGetCountry() {
-            (new swagger.LocationApi())
-                .locationCountryGet(getToken())
-                .then(response => getCountryList(response));
-        }
-        requestToGetCountry();
-
-        $(document).on('change','.select-country',function() {
-            $(".select-province").prop('disabled', false);
-            var countryID = $(this).val();
-            if(countryID){
-                (new swagger.LocationApi())
-                    .locationProvinceGet(getToken())
-                    .then(response => getProvinceList(response));
-            }
-        });
-        $(document).on('change','.select-province',function() {
-            $(".select-city").prop('disabled', false);
-            let provinceID = $(this).val();
-            if(provinceID){
-                (new swagger.LocationApi())
-                    .locationCityIdGet(provinceID,getToken())
-                    .then(response => getCityList(response));
-            }
-        })
     }
 
     render() {
-        return (<SelectLocationPTR callCityName={this.callCityName} callProvinceName={this.callProvinceName} />);
+        return (<SelectLocationPTR onProvinceChanged={this.onProvinceChanged.bind(this)}
+                                   onCountryChanged={this.onCountryChanged.bind(this)} {...this.props} {...this.state}/>);
     }
 }
