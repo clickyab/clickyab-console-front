6b15e1da77a35ba05ef16244f794da9822c07393
hasan gilak <hasan.gilak@outlook.com>
2017-01-22 16:32:45 +0330

    selectaddtype

--- src/app/components/campaign/select-type/SelectTypePTR.jsx
+++ src/app/components/campaign/select-type/SelectTypePTR.jsx
@@ -1,27 +1,26 @@
 import React, {Component} from "react";
 import $ from "jquery";
-import {Field, reduxForm} from "redux-form";
+import {reduxForm, change} from "redux-form";
 import Radio from './../../common/form/Radio';
+import {dispatch} from "../../../functions/dispatch";
 
 
 class SelectTypePTR extends Component {
     selectTypeContentForm;
 
-    // handleInitialize() {
-    //     const initData = select("user.personal", {}, true);
-    //     initData.email = getEmail();
-    //     this.props.initialize(initData);
-    // }
+    handleInitialize() {
+        dispatch(change("SelectTypePTRForm", 'select-type-content', 'generate_content'))
+    }
 
     componentDidMount() {
-        // this.handleInitialize();
-        $('input[name=select-type-content]').change(function(event) {
-            if ( $(this).is(':checked') && ($(this).val() == 'generate_content')) {
-                $(".when-select-content").fadeOut("normal" , function () {
+        this.handleInitialize();
+        $('input[name=select-type-content]').change(function (event) {
+            if ($(this).is(':checked') && ($(this).val() == 'generate_content')) {
+                $(".when-select-content").fadeOut("normal", function () {
                     $(".when-generate-content").fadeIn();
                 });
-            } else if ( $(this).is(':checked') && ($(this).val() == 'select_content')) {
-                $(".when-generate-content").fadeOut("normal" , function () {
+            } else if ($(this).is(':checked') && ($(this).val() == 'select_content')) {
+                $(".when-generate-content").fadeOut("normal", function () {
                     $(".when-select-content").fadeIn();
                 });
             }
@@ -51,7 +50,8 @@ class SelectTypePTR extends Component {
 
                     <div className="portlet-title">
                         <div className="caption">
-                            <i className="fa fa-bullseye"/> ثبت کمپین جدید </div>
+                            <i className="fa fa-bullseye"/> ثبت کمپین جدید
+                        </div>
                     </div>
                     <div className="portlet-body form">
                         <div className="mt-element-step margin-top-20 when-select-content ">
@@ -59,44 +59,57 @@ class SelectTypePTR extends Component {
                                 <div className="col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۱</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">نام کمپین</div>
-                                    <div className="mt-step-content font-grey-cascade">یک نام به کمپین خود اختصاص دهید.</div>
+                                    <div className="mt-step-content font-grey-cascade">یک نام به کمپین خود اختصاص
+                                        دهید.
+                                    </div>
                                 </div>
                                 <div className="col-md-3 bg-grey-steel mt-step-col active">
                                     <div className="mt-step-number">۲</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">انتخاب نوع محتوا</div>
-                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان بازدید مشخص کنید</div>
+                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان
+                                        بازدید مشخص کنید
+                                    </div>
                                 </div>
                                 <div className="col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۳</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">انتخاب محتوا</div>
-                                    <div className="mt-step-content font-grey-cascade">انتخاب محتوای کانال های دیگر</div>
+                                    <div className="mt-step-content font-grey-cascade">انتخاب محتوای کانال های دیگر
+                                    </div>
                                 </div>
                                 <div className="col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۴</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">انتخاب پلن</div>
-                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان بازدید مشخص کنید</div>
+                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان
+                                        بازدید مشخص کنید
+                                    </div>
                                 </div>
 
                             </div>
                             <br/>
                         </div>
 
-                        <div className="mt-element-step margin-top-20 when-generate-content" style={{display:'none'}}>
+                        <div className="mt-element-step margin-top-20 when-generate-content" style={{display: 'none'}}>
                             <div className="row step-background">
                                 <div className="col-lg-15 col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۱</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">نام کمپین</div>
-                                    <div className="mt-step-content font-grey-cascade">یک نام به کمپین خود اختصاص دهید.</div>
+                                    <div className="mt-step-content font-grey-cascade">یک نام به کمپین خود اختصاص
+                                        دهید.
+                                    </div>
                                 </div>
                                 <div className="col-lg-15 col-md-3 bg-grey-steel mt-step-col active">
                                     <div className="mt-step-number">۲</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">انتخاب نوع محتوا</div>
-                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان بازدید مشخص کنید</div>
+                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان
+                                        بازدید مشخص کنید
+                                    </div>
                                 </div>
                                 <div className=" col-lg-15 col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۳</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">آپلود فایل و مدیا</div>
-                                    <div className="mt-step-content font-grey-cascade">عکس، ویدیو و یا پی دی اف خود را آپلود کنید.</div>
+                                    <div className="mt-step-content font-grey-cascade">عکس، ویدیو و یا پی دی اف خود را
+                                        آپلود کنید.
+                                    </div>
                                 </div>
                                 <div className="col-lg-15 col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۴</div>
@@ -106,7 +119,9 @@ class SelectTypePTR extends Component {
                                 <div className="col-lg-15 col-md-3 bg-grey-steel mt-step-col">
                                     <div className="mt-step-number">۵</div>
                                     <div className="mt-step-title uppercase font-grey-cascade">انتخاب پلن</div>
-                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان بازدید مشخص کنید</div>
+                                    <div className="mt-step-content font-grey-cascade">انتخاب پلن را بر اساس میزان
+                                        بازدید مشخص کنید
+                                    </div>
                                 </div>
 
                             </div>
@@ -116,15 +131,24 @@ class SelectTypePTR extends Component {
                             <form>
                                 <div className='col-md-12 col-xs-12'>
                                     <div className='mt-element-ribbon bg-grey-steel'>
-                                        <div className='ribbon ribbon-color-primary uppercase'>نوع محتوای تبلیغ خود را مشخص کنید</div>
+                                        <div className='ribbon ribbon-color-primary uppercase'>نوع محتوای تبلیغ خود را
+                                            مشخص کنید
+                                        </div>
                                         <div className='ribbon-content'>
-                                            شما می توانید برای کمپین خود هم محتوای جدید تولید کنید  و یا همچنین می توانید محتوای کانال های دیگران را <b>پروموت</b> کنید ، در صورتی که پروموت را انتخاب نمایید می توانید یکی از پیام های کانل ها را انتخاب کرده و کمپین خود را نهایی کنید و یا اگر میخواهید محتوای جدید تولید کنید می تواند با انتخاب گزینه <b>محتوای جدید</b> متن و و مدیای خود را آپلود نمایید
+                                            شما می توانید برای کمپین خود هم محتوای جدید تولید کنید و یا همچنین می توانید
+                                            محتوای کانال های دیگران را <b>پروموت</b> کنید ، در صورتی که پروموت را انتخاب
+                                            نمایید می توانید یکی از پیام های کانل ها را انتخاب کرده و کمپین خود را نهایی
+                                            کنید و یا اگر میخواهید محتوای جدید تولید کنید می تواند با انتخاب گزینه <b>محتوای
+                                            جدید</b> متن و و مدیای خود را آپلود نمایید
                                             <br />
                                             <br />
                                             <div className='form-group form-md-radios'>
                                                 <div className='md-radio-list'>
-                                                    <Radio label={'محتوای جدید'} name={'select-type-content'} value={'generate_content'} id={'generate_content'}/>
-                                                    <Radio label={'پروموت محتوای کانال های دیگر'} name={'select-type-content'} value={'select_content'} id={'select_content'}/>
+                                                    <Radio label={'محتوای جدید'} name={'select-type-content'}
+                                                           value={'generate_content'} id={'generate_content'}/>
+                                                    <Radio label={'پروموت محتوای کانال های دیگر'}
+                                                           name={'select-type-content'} value={'select_content'}
+                                                           id={'select_content'}/>
                                                 </div>
                                             </div>
                                         </div>
--- src/app/components/common/form/Radio.jsx
+++ src/app/components/common/form/Radio.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Field} from 'redux-form';
 
-export default ({name, id, label, value , checked, ...props}) => {
+export default ({name, id, label, value , ...props}) => {
     return (
         <div className="md-radio">
-            <Field name={name} value={value}  type="radio" defaultChecked component="input"
+            <Field name={name} value={value}  type="radio" component="input"
                    id={id} className="md-radiobtn" />
             <label htmlFor={id}>
                 <span/>
