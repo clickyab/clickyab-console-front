5147cc827e04da5049849fea0851f0e1662ae8b5
kasra <kasra.ansari@yahoo.com>
2017-01-23 16:28:26 +0330

    add create campaign button & navigate to step

--- src/app/components/campaign/CampaignListPTR.jsx
+++ src/app/components/campaign/CampaignListPTR.jsx
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 import DataTable from '../common/DataTable/DataTable';
+import {navigate} from "../../functions/navigate";
 
 export default class CampaignListPTR extends Component {
-	render() {
+	componentDidMount() {
+		$(document).on("click", "#showAddCampaign", function () {
+			navigate('/v1/campaign/create/step/name');
+		})
+	}
 
+	render() {
 		return (
 			<div className='page-content'>
+				<div className='row'>
+					<div className='col-lg-8 col-md-8 col-sm-8 col-xs-12'>
+						<h1 className='page-title'> مدیریت کمپین‌ها</h1>
+					</div>
+					<div className='top-action-header'>
+						<button className="btn btn-lg blue pull-left" id="showAddCampaign">
+							<i className="fa fa-plus"/> ساخت کمپین‌ جدید
+						</button>
+					</div>
+				</div>
 				<div className='portlet light datatable-parent'>
 					<div className='portlet-title'>
 						<div className='caption'>
