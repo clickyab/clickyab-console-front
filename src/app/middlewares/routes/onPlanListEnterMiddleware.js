4a6e6472877b419c767a867037be682d68195fdf
milad <miladheydari.work@gmail.com>
2017-01-24 10:55:48 +0330

    middleware plan

--- src/app/middlewares/routes/onPlanListEnterMiddleware.js
+++ src/app/middlewares/routes/onPlanListEnterMiddleware.js
@@ -1,16 +1,43 @@
 import {sync} from '../../functions/sync';
+import * as swagger from '../../swagger/index';
+import {dispatch} from '../../functions/dispatch';
+import {select} from '../../functions/select';
 import {loading} from '../../functions/loading';
 import {isLoginMiddleware} from "../isLoginMiddleware";
+import {planListAction} from "../../redux/actions/index";
+import {throwError} from "../../functions/Error";
+import {navigate} from "../../functions/navigate";
+import {raceOnTime} from "../../functions/raceOnTime";
 import {handleError} from "../../functions/catchError";
+
+function* planListController(done) {
+    loading(true);
+    yield* isLoginMiddleware();
+    const {error, data} = yield (new swagger.PlanApi())
+        .planGet(select('user.token'));
+
+    done();
+    if (!error) {
+        dispatch(planListAction(data));
+        loading(false);
+    } else {
+        throwError("onChannelEnterMiddleWare", function () {
+            navigate('/v1/login');
+        });
+    }
+}
 
 export default (nextState, replace, next) => sync(function*() {
     try {
-        loading(true);
         yield* isLoginMiddleware();
-        loading(false);
-
+        let {error} = yield raceOnTime(planListController, 20000);
+        if(error)
+            navigate('/v1/login');
         next();
     } catch (error) {
         handleError(error);
     }
 });
+
+
+
