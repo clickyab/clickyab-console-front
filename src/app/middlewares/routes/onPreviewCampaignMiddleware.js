89fb276131456088aceff4127624974a07df9cad
kasra <kasra.ansari@yahoo.com>
2017-01-25 17:18:33 +0330

    preview page

---
+++ src/app/middlewares/routes/onPreviewCampaignMiddleware.js
@@ -0,0 +1,16 @@
+import {sync} from '../../functions/sync';
+import {loading} from '../../functions/loading';
+import {isLoginMiddleware} from "../isLoginMiddleware";
+import {handleError} from "../../functions/catchError";
+
+export default (nextState, replace, next) => sync(function*() {
+    try {
+        loading(true);
+        yield* isLoginMiddleware();
+        loading(false);
+
+        next();
+    } catch (error) {
+        handleError(error);
+    }
+});
