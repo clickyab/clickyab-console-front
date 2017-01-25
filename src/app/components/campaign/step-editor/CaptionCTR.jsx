a3ccd55ae451c72dec053f0a413936f6ddec5400
Milad Heydari <miladheydari.work@gmail.com>
2017-01-25 17:58:50 +0330

    Campaign final (#91)
    * select-type
    
    * select-type
    
    * :|
    
    * campaign
    
    * fix bug

--- src/app/components/campaign/step-editor/CaptionCTR.jsx
+++ src/app/components/campaign/step-editor/CaptionCTR.jsx
@@ -21,33 +21,34 @@ export default class CaptionCTR extends Component {
         let textarea_text = $(".emojieditor-plugin-content");
         $(document).on("click", ".caption-text-form-btn", function (e) {
             e.preventDefault();
-            sync(function*() {
-                loadingProgress = Ladda.create(document.querySelector('button.caption-text-form-btn'));
-                loadingProgress.start();
+            if($(".emojionearea-editor").text != "") {
+                sync(function*() {
+                    loadingProgress = Ladda.create(document.querySelector('button.caption-text-form-btn'));
+                    loadingProgress.start();
 
-                const {response} = yield (new swagger.AdApi())
-                    .campaignDescIdPut(select("createCampaignData.id", "no id"), select("user.token", "no token"), {'payloadData': {"body": textarea_text.val()}});
+                    const {response} = yield (new swagger.AdApi())
+                        .campaignDescIdPut(select("createCampaignData.id", "no id"), select("user.token", "no token"), {'payloadData': {"body": textarea_text.val()}});
 
-                response.error = 'اطلاعات شما صحیح نمی‌باشد.';
-                response.text = 'اطلاعات شما با موفقیت ثبت شد.';
-                if (response.statusCode == '200') {
-                    dispatch(createCampaign(Object.assign({}, select("createCampaignData"), {description: textarea_text.val()})));
-                    dispatch(deleteCampaignPromote())
-                    dispatch(updateLocalStorageAction());
+                    response.error = 'اطلاعات شما صحیح نمی‌باشد.';
+                    response.text = 'اطلاعات شما با موفقیت ثبت شد.';
+                    if (response.statusCode == '200') {
+                        dispatch(createCampaign(Object.assign({}, select("createCampaignData"), {description: textarea_text.val()})));
+                        dispatch(updateLocalStorageAction());
 
-                    loadingProgress.stop();
-                    AlertBox("success", "کپشن با موفقیت افزوده شد، هم اکنون پلن تبلیغاتی خود را انتخاب کنید");
+                        loadingProgress.stop();
+                        AlertBox("success", "کپشن با موفقیت افزوده شد، هم اکنون پلن تبلیغاتی خود را انتخاب کنید");
 
-                    navigate('/v1/campaign/create/:campaign_id:/step/plan', {
-                        campaign_id: select('createCampaignData.id')
-                    });
-                } else if (response.statusCode == '400') {
-                    loadingProgress.stop();
-                    FailedBoxAlert(response);
-                }
+                        navigate('/v1/campaign/create/:campaign_id:/step/plan', {
+                            campaign_id: select('createCampaignData.id')
+                        });
+                    } else if (response.statusCode == '400') {
+                        loadingProgress.stop();
+                        FailedBoxAlert(response);
+                    }
 
-                ifInvalidToken(response);
-            });
+                    ifInvalidToken(response);
+                });
+            }
         });
     }
 
