52198f3645559f96d9937015b749585773bdb8d3
hasan gilak <hasan.gilak@outlook.com>
2017-02-01 15:16:15 +0330

    fixed

--- src/app/components/telegram/TelegramListCTR.jsx
+++ src/app/components/telegram/TelegramListCTR.jsx
@@ -6,8 +6,23 @@ import swagger from './../../swagger/index';
 import {select} from "../../functions/select";
 import {telegramItemsListAction} from "../../redux/actions/index";
 import moment from "moment-jalali";
+import {translatable} from 'react-multilingual/dist';
 
 @connect(({telegramList}) => ({telegramList}))
+@translatable(({
+    ID, Name, Description,
+    CreatedAt, UpdatedAt, Action,
+    Email, UserID, BotChatID, Username,
+    Resolve, Remove
+
+}) => ({
+    translation: {
+        ID, Name, Description,
+        CreatedAt, UpdatedAt, Action,
+        Email, UserID, BotChatID, Username,
+        Resolve, Remove
+    }
+}))
 export default class TelegramListCTR extends Component {
     callApi(query_name, value) {
         let {dispatch} = this.props;
@@ -54,6 +69,10 @@ export default class TelegramListCTR extends Component {
         this.callApi('c', per_page);
     }
 
+    translator(title) {
+        return this.props.translation[title];
+    }
+
     render() {
         return (<TelegramListPTR {...this.props.telegramList}
                                  sort={this.sort.bind(this)}
@@ -61,8 +80,9 @@ export default class TelegramListCTR extends Component {
                                  search={this.search.bind(this)}
                                  onPaginationChange={this.onPaginationChange.bind(this)}
                                  onPerPageChange={this.onPerPageChange.bind(this)}
-                                //mutators={{updated_at: this.updated_at, created_at: this.created_at}}
+                                 mutators={{updated_at: this.updated_at, created_at: this.created_at}}
                                  edit={this.edit.bind(this)}
+                                 translator={this.translator.bind(this)}
         />)
     }
 }
