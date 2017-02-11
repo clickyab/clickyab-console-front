1d80007cacee6665a701ace7f52787ca4b1137ea
hassan gilak <hasan.gilak@outlook.com>
2017-02-11 21:11:49 +0330

    ping

--- src/app/functions/ping.js
+++ src/app/functions/ping.js
@@ -1,6 +1,19 @@
 import swagger from "../swagger/index";
 import {select} from "./select";
+import moment from "moment";
 
+let lastTimeWePinged = null;
 export default function ping() {
-    return new swagger.UserApi().userPingGet(select('user.token', 'no token'));
+	if (lastTimeWePinged === null) {
+		lastTimeWePinged = new Date();
+		return new swagger.UserApi().userPingGet(select('user.token', 'no token'));
+	}
+
+	if (moment(new Date()).isAfter(moment(lastTimeWePinged).add(15, 'm'))) {
+		return new swagger.UserApi().userPingGet(select('user.token', 'no token'));
+	}
+
+	return new Promise((resolve) => {
+		resolve({data: select("user"), response: {statusCode: 200}});
+	});
 }
