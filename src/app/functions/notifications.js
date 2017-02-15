a638a3126b45ec6590bcbf418044fd8bc9a6c1f1
hassan gilak <hasan.gilak@outlook.com>
2017-02-15 17:43:10 +0330

    alerts

--- src/app/functions/notifications.js
+++ src/app/functions/notifications.js
@@ -1,39 +1,40 @@
 let Lobibox = require('./../../../public/vendor/lobibox');
+
 export function AlertBox(type, msg) {
-    Lobibox.notify(
-        type,
-        {
-            size: 'mini',
-            rounded: false,
-            sound: false,
-            icon: false,
-            delay: 5000,
-            position: 'top right',
-            delayIndicator: true,
-            msg: msg
-        }
-    );
+	return Lobibox.notify(
+		type,
+		{
+			size: 'mini',
+			rounded: false,
+			sound: false,
+			icon: false,
+			delay: 5000,
+			position: 'top right',
+			delayIndicator: true,
+			msg: msg
+		}
+	);
 }
 
 export function SuccessBoxAlert(response) {
-    AlertBox('success', response.text);
+	AlertBox('success', response.text);
 }
 export function FailedBoxAlert(response) {
-    AlertBox('error', response.error);
+	AlertBox('error', response.error);
 }
 
 export function NotifyBox(type, msg, delay) {
-    Lobibox.notify(
-        type, {
-            size: 'mini',
-            rounded: false,
-            sound: false,
-            icon: false,
-            delay: delay,
-            position: 'top right',
-            delayIndicator: true,
-            msg: msg
-        }
-    )
+	return Lobibox.notify(
+		type, {
+			size: 'mini',
+			rounded: false,
+			sound: false,
+			icon: false,
+			delay: delay,
+			position: 'top right',
+			delayIndicator: true,
+			msg: msg
+		}
+	)
 }
 
