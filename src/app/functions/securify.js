d68b8dca54b9b8cdb0e31e6a3879cd86a5a1beef
kasra <kasra.ansari@yahoo.com>
2017-02-21 14:16:44 +0330

    permission check for url

---
+++ src/app/functions/securify.js
@@ -0,0 +1,13 @@
+import {secure} from "react-redux-secure";
+import React, {Component} from "react";
+
+export function securify(InSecuredComponent, secureCallback) {
+    @secure(secureCallback)
+    class SecuredComponent extends Component {
+        render() {
+            return <InSecuredComponent/>
+        }
+    }
+
+    return <SecuredComponent/>;
+}
