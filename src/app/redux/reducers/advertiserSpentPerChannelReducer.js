0ad27a8c7869aabd108134ab75aeb944814f6981
kasra <kasra.ansari@yahoo.com>
2017-02-26 18:02:39 +0330

    dashboard chart data

---
+++ src/app/redux/reducers/advertiserSpentPerChannelReducer.js
@@ -0,0 +1,10 @@
+import {ADVERTISER_SPENT_PER_CHANNEL} from "../actions/index";
+
+export function advertiserSpentPerChannelReducer(state = [], action) {
+    switch (action.type) {
+        case ADVERTISER_SPENT_PER_CHANNEL:
+            return action.data;
+    }
+
+    return state;
+}
