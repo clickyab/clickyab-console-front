ac991d8281a5b7075d315d3b157950a488bfbacb
kasra <kasra.ansari@yahoo.com>
2017-02-27 16:09:07 +0330

    before create campaign user must fill profile
    fix #999

--- src/app/middlewares/routes/onCreateCampaignStepOneOnEnterMiddleware.js
+++ src/app/middlewares/routes/onCreateCampaignStepOneOnEnterMiddleware.js
@@ -4,11 +4,41 @@ import {isLoginMiddleware} from "../isLoginMiddleware";
 import {handleError} from "../../functions/catchError";
 import {dispatch} from "../../functions/dispatch";
 import {createCampaign} from "../../redux/actions/index";
+import {select} from "../../functions/select";
+import {navigate} from "../../functions/navigate";
+let swal = require('sweetalert');
+
+function checkProfile() {
+    if ((select('user.user_id') && (select('user.personal') || select('user.corporation')) == null) == true) {
+        swal({
+                title: "لطفا قبل از افزودن کمپین، اطلاعات حساب کاربری خود را تکمیل نمایید",
+                text: "",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#337ab7",
+                confirmButtonText: "باز کردن صفحه حساب کاربری",
+                cancelButtonText: "انصراف!",
+                closeOnConfirm: true,
+                showLoaderOnConfirm: false,
+                closeOnCancel: true,
+                html: true
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    navigate('/v1/profile');
+                } else {
+                    navigate('/v1/advertiser');
+                }
+            });
+
+    }
+}
 
 export default (nextState, replace, next) => sync(function*() {
     try {
         loading(true);
         // yield* isLoginMiddleware();
+        checkProfile();
         dispatch(createCampaign({}));
         loading(false);
         next();
