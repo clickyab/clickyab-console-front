34adc1d6f96616b34e832ce712f8159bbc4fc904
hasan gilak <hasan.gilak@outlook.com>
2017-03-12 14:24:27 +0330

    we have missed logic on version midlleware that caused out revovery password page redirect to login page. fix #1178

--- src/app/components/PaswordRecovery/PasswordRecoveryPTR.jsx
+++ src/app/components/PaswordRecovery/PasswordRecoveryPTR.jsx
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import $ from "jquery";
 import {Field, reduxForm} from "redux-form";
 import {Link} from "react-router";
-import {RouteTransition} from "react-router-transition";
 import {translatable} from "react-multilingual/dist";
 
 
@@ -29,7 +28,6 @@ class PasswordRecoveryPTR extends Component {
     };
 
     componentDidMount() {
-
         this.form = $("form");
         this.form.validate({
             rules: {
@@ -45,66 +43,66 @@ class PasswordRecoveryPTR extends Component {
     }
 
     render() {
-
         let {
             email, forget_submit, do_you_have_account, login_now, please_enter_your_mail_to_send_new_password, forgotPassTitle
         } = this.props;
         const {handleSubmit, SubmitCall} = this.props;
+        
         return (
-                <div className="top-content auth-pages">
-                    <img src="/bg.jpg" className="fullscreen-back"/>
-                    <div className="inner-bg">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-sm-8 col-sm-offset-2 text">
-                                    <h1>
-                                        <a href="http://rubikad.com">
-                                            <img src="/img/logo_fa_white.svg" width="300px"/>
-                                        </a>
-                                    </h1>
-                                </div>
+            <div className="top-content auth-pages">
+                <img src="/bg.jpg" className="fullscreen-back"/>
+                <div className="inner-bg">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-8 col-sm-offset-2 text">
+                                <h1>
+                                    <a href="http://rubikad.com">
+                                        <img src="/img/logo_fa_white.svg" width="300px"/>
+                                    </a>
+                                </h1>
                             </div>
-
-                            <div className="row">
-                                <div className="col-sm-6 col-sm-offset-3 form-box">
-                                    <div className="form-top">
-                                        <div className="form-top-left">
-                                            <h3>{forgotPassTitle}</h3>
-                                            <p>{please_enter_your_mail_to_send_new_password}</p>
-                                        </div>
-                                        <div className="form-top-right">
-                                            <i className="fa fa-rocket"/>
-                                        </div>
-                                    </div>
-                                    <div className="form-bottom">
-                                        <form role="form" action="" method="post" className="recovery-password-form"
-                                              onSubmit={handleSubmit((values) => SubmitCall(values, this.form))}>
-                                            <div className="form-group">
-                                                <label className="sr-only" htmlFor="form-username">{email}</label>
-                                                <Field component="input" type="text" name="email" placeholder={email}
-                                                       className="form-username form-control" id="email"
-                                                       style={{direction: 'ltr', textAlign: 'left'}}/>
-                                            </div>
-                                            <button type="submit" className="btn mt-ladda-btn ladda-button btn-primary"
-                                                    data-style="zoom-in"><span
-                                                className="ladda-label">{forget_submit}</span></button>
-                                        </form>
-                                        <div className="success-message-recovery text-center" style={{display: 'none'}}>
-                                            <h3>ایمیل بازیابی کلمه عبور با موفقیت ارسال شد</h3>
-                                            <p>لطفا بخش BULK یا SPAM ایمیل خود را نیز چک کنید</p>
-                                        </div>
-                                    </div>
-                                    <div className="panel-footer">
-                                        {do_you_have_account} <Link to="/v1/login">{login_now}</Link>
-                                    </div>
-
-                                </div>
-                            </div>
-
                         </div>
-                    </div>
 
+                        <div className="row">
+                            <div className="col-sm-6 col-sm-offset-3 form-box">
+                                <div className="form-top">
+                                    <div className="form-top-left">
+                                        <h3>{forgotPassTitle}</h3>
+                                        <p>{please_enter_your_mail_to_send_new_password}</p>
+                                    </div>
+                                    <div className="form-top-right">
+                                        <i className="fa fa-rocket"/>
+                                    </div>
+                                </div>
+                                <div className="form-bottom">
+                                    <form role="form" action="" method="post" className="recovery-password-form"
+                                          onSubmit={handleSubmit((values) => SubmitCall(values, this.form))}>
+                                        <div className="form-group">
+                                            <label className="sr-only" htmlFor="form-username">{email}</label>
+                                            <Field component="input" type="text" name="email" placeholder={email}
+                                                   className="form-username form-control" id="email"
+                                                   style={{direction: 'ltr', textAlign: 'left'}}/>
+                                        </div>
+                                        <button type="submit" className="btn mt-ladda-btn ladda-button btn-primary"
+                                                data-style="zoom-in"><span
+                                            className="ladda-label">{forget_submit}</span></button>
+                                    </form>
+                                    <div className="success-message-recovery text-center" style={{display: 'none'}}>
+                                        <h3>ایمیل بازیابی کلمه عبور با موفقیت ارسال شد</h3>
+                                        <p>لطفا بخش BULK یا SPAM ایمیل خود را نیز چک کنید</p>
+                                    </div>
+                                </div>
+                                <div className="panel-footer">
+                                    {do_you_have_account} <Link to="/v1/login">{login_now}</Link>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+
+            </div>
         );
     }
 }
