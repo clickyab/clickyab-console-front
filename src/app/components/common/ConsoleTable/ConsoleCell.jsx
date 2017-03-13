0de8dd22cac823cd8602a707c2a687ca1fc7ece4
hasan gilak <hasan.gilak@outlook.com>
2017-03-13 15:45:34 +0330

    deposit error fixed

--- src/app/components/common/ConsoleTable/ConsoleCell.jsx
+++ src/app/components/common/ConsoleTable/ConsoleCell.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from "react";
 
 export class ConsoleCell extends Component {
+<<<<<<< Updated upstream
     buildActions(id, item, edit) {
+=======
+    buildActions(id, item) {
+        let {depositAction, edit} = this.props;
+>>>>>>> Stashed changes
         const actionKeys = item.split(',');
         let buttons = [];
         for (let i = 0; i < actionKeys.length; i++) {
             if (actionKeys[i] == "edit") {
                 buttons.push(edit(id));
+<<<<<<< Updated upstream
+=======
+            } else if (actionKeys[i] == "deposit") {
+                buttons.push(depositAction(id));
+>>>>>>> Stashed changes
             }
         }
 
@@ -14,7 +24,12 @@ export class ConsoleCell extends Component {
     }
 
     render() {
+<<<<<<< Updated upstream
         let {mutator, data, actions, edit, id} = this.props;
+=======
+        let {mutator, data, actions, id} = this.props;
+
+>>>>>>> Stashed changes
         return (
             actions ? <td>{this.buildActions(id, data, edit)}</td> : mutator ? <td>{mutator(data, this.props)}</td> :
                     <td>{data}</td>
--- src/app/components/common/ConsoleTable/ConsoleTable.jsx
+++ src/app/components/common/ConsoleTable/ConsoleTable.jsx
@@ -142,7 +142,11 @@ export class ConsoleTable extends Component {
     }
 
     render() {
+<<<<<<< Updated upstream
         let {sort, filter, list, change, edit, search, mutators, translator} = this.props;
+=======
+        let {sort, filter, list, change, edit, depositAction, search, mutators, translator} = this.props;
+>>>>>>> Stashed changes
 
         return (
             <div className="table-holder">
@@ -214,6 +218,10 @@ export class ConsoleTable extends Component {
                                                                             mutator={mutators[header.data]}
                                                                             actions={header.data == "_actions"}
                                                                             change={change} edit={edit}
+<<<<<<< Updated upstream
+=======
+                                                                            depositAction={depositAction}
+>>>>>>> Stashed changes
                                                                             className="value-datatable"
                                                                             rowIndex={rowIndex} {...cell}/>
                                 )}
