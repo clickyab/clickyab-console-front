daf158ddc1d4690f9488684f5063a53616f14286
hasan gilak <hasan.gilak@outlook.com>
2017-04-04 16:37:57 +0430

    fixed the hard way

--- src/app/middlewares/routes/onSlashEnterMiddleware.js
+++ src/app/middlewares/routes/onSlashEnterMiddleware.js
@@ -2,14 +2,13 @@ import {sync} from "../../functions/sync";
 import {loading} from "../../functions/loading";
 import {handleError} from "../../functions/catchError";
 import {redirectIfLogin} from "../redirectIfLogin";
-import {navigate} from "../../functions/navigate";
 
 export default () => sync(function*() {
     try {
         loading(true);
         yield* redirectIfLogin('/v1/advertiser');
 
-        navigate('/v1/login');
+        window.location = '/v1/login';
     } catch (error) {
         handleError(error);
     }
