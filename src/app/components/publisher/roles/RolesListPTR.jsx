accb99d44b36aa502b5acdd0ee441a9525b455c5
kasra <kasra.ansari@yahoo.com>
2017-04-04 18:01:56 +0430

    secure almost done

--- src/app/components/publisher/roles/RolesListPTR.jsx
+++ src/app/components/publisher/roles/RolesListPTR.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {ConsoleTable} from "../../common/ConsoleTable/ConsoleTable";
 import EditRoleModalCTR from "./EditRoleModalCTR";
 import AddRoleModalCTR from "./AddRoleModalCTR";
+import {securify} from "../../../functions/securify";
 
 export default class RolesListPTR extends Component {
     componentDidMount() {
@@ -23,17 +24,25 @@ export default class RolesListPTR extends Component {
                         <div className='caption'>
                             <span className='caption-subject bold uppercase font-dark'>لیست چنل ها </span>
                         </div>
-                        <div className="actions">
-                            <div className="btn-group btn-group-devided" data-toggle="buttons">
-                                <button className="btn btn-transparent blue btn-outline btn-circle btn-sm"
-                                        id="showAddRoleModalForm">ساخت رول جدید
-                                </button>
-                            </div>
-                        </div>
+                        {securify(
+                            () => <div className="actions">
+                                <div className="btn-group btn-group-devided" data-toggle="buttons">
+                                    <button className="btn btn-transparent blue btn-outline btn-circle btn-sm"
+                                            id="showAddRoleModalForm">ساخت رول جدید
+                                    </button>
+                                </div>
+                            </div>,
+                            ({user}, {canSeeCreateRole}, run) => run(canSeeCreateRole())
+                        )}
+
                     </div>
-                    <div className='portlet-body'>
-                        <ConsoleTable {...this.props} list="role"/>
-                    </div>
+                    {securify(
+                        () => <div className='portlet-body'>
+                            <ConsoleTable {...this.props} list="role"/>
+                        </div>,
+                        ({user}, {canSeeRoleList}, run) => run(canSeeRoleList())
+                    )}
+
                 </div>
                 <AddRoleModalCTR permissions={this.props.permissions}/>
                 <EditRoleModalCTR permissions={this.props.permissions} form="EditRoleModalPTR"/>
