9427e4aeb8ec95b928d7fcde742cf9f32540ab64
kasra <kasra.ansari@yahoo.com>
2017-04-08 13:47:14 +0430

    fix translate function and translate side bar

--- src/app/functions/translate.js
+++ src/app/functions/translate.js
@@ -5,7 +5,7 @@ import swagger from "./../swagger/index";
 import {sprintf} from "sprintf-js";
 
 export function translate(text, ...parameters) {
-	let translated = select('translations', {})[text];
+	let translated = select('translations', {})[select('locale')][text];
 	if (translated) {
 		return sprintf(translated, ...parameters);
 	}
