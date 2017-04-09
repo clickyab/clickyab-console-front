a177a218483ae89737b19af259694af999b010d3
hassan gilak <hasan.gilak@outlook.com>
2017-04-09 14:43:30 +0430

    translation with html embede

--- src/app/functions/translate.js
+++ src/app/functions/translate.js
@@ -3,7 +3,7 @@ import {sync} from "./sync";
 import {getToken} from "../redux/helpers";
 import swagger from "./../swagger/index";
 import {sprintf} from "sprintf-js";
-
+import React from "react";
 export function translate(text, ...parameters) {
 	let translated = select('translations', {})[select('locale')][text];
 
@@ -19,4 +19,8 @@ export function translate(text, ...parameters) {
 	});
 
 	return sprintf(translated, ...parameters);
+}
+
+export function translateViaHtml(text, ...parameters) {
+	return <span dangerouslySetInnerHTML={{__html:translate(text, ...parameters)}} />
 }
