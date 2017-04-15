8be8077794e9631459ac0b66bead109e2245cba0
milad <miladheydari.work@gmail.com>
2017-04-15 16:50:10 +0430

    parse sessiogn list

--- src/app/components/UserProfile/ProfileSidebarPTR.jsx
+++ src/app/components/UserProfile/ProfileSidebarPTR.jsx
@@ -25,22 +25,10 @@ export default class ProfileSidebarPTR extends Component {
 					<div className="profile-usertitle">
 						<div className="profile-usertitle-name"> {initData}  </div>
 					</div>
-					<div className="profile-userbuttons">
+					<div className="profile-userbuttons" style={{paddingBottom: '30px'}}>
 						<button type="button" className="btn btn-circle red btn-sm logout-btn"
 								onClick={this.props.SubmitLogout.bind(this)}>خروج
 						</button>
-					</div>
-					<div className="profile-usermenu">
-						<ul className="nav">
-							<li>
-								<a href="page_user_profile_1.html">
-									<i className="icon-home"/> شارژ حساب </a>
-							</li>
-							<li className="">
-								<a href="page_user_profile_1_account.html">
-									<i className="icon-settings"/> برداشت حساب </a>
-							</li>
-						</ul>
 					</div>
 				</div>
 			</div>
