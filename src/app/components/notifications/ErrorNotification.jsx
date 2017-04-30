8d367fb7c59c2829a80401376ab61161a9027957
kasra <kasra.ansari@yahoo.com>
2017-04-30 14:49:48 +0430

    change notification to react
    fix #1435

--- src/app/components/notifications/ErrorNotification.jsx
+++ src/app/components/notifications/ErrorNotification.jsx
@@ -1,20 +1,37 @@
-import React, {Component, PropTypes} from "react";
+import React, {Component} from "react";
+import {Trash} from "./NotificationsDropDown";
 
 export default class ErrorNotification extends Component {
+    state = {
+        trash: <span/>
+    };
+
     onClick() {
         let {onAnEventSeenClick, notification: {id}} = this.props;
 
         onAnEventSeenClick(id);
     }
 
+    showTrash() {
+        this.setState({trash: <Trash/>});
+    }
+
+    hideTrash() {
+        this.setState({trash: <span/>});
+    }
+
     render() {
         let {count, notification: {message}} = this.props;
+        let {trash} = this.state;
 
         return (
-            <div>
+            <div className="menu-list-custom-block" onMouseLeave={this.hideTrash.bind(this)}
+                 onMouseEnter={this.showTrash.bind(this)}>
                 <span className="label label-sm label-icon label-danger"><i className="fa fa-times"/></span>
-                <span className="title">{count} - {message}</span>
-                <span className="time" onClick={this.onClick.bind(this)}/>
+                <span className="title">{count} - {message}  </span>
+                <span className="time" onClick={this.onClick.bind(this)}>
+                    {trash}
+                </span>
             </div>
         );
     }
