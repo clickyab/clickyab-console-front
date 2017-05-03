aac64ad21c84954a0c1639210b0545b117f6842f
kasra <kasra.ansari@yahoo.com>
2017-05-03 14:33:26 +0430

    change profile sidebar to react
    fix #1431

--- src/app/components/UserProfile/ProfileSidebarPTR.jsx
+++ src/app/components/UserProfile/ProfileSidebarPTR.jsx
@@ -2,11 +2,11 @@ import React, {Component, PropTypes} from "react";
 import {getGravatarFromEmail} from "../../functions/gravatar";
 import {getCorporationTitle, getEmail, getFullName} from "./../../redux/helpers";
 import {select} from "./../../functions/select";
+import {translate} from './../../functions/translate'
 
 export default class ProfileSidebarPTR extends Component {
-	componentDidMount() {
-		let userAvatar = getGravatarFromEmail(getEmail(), 200);
-		document.querySelector(".profile-userpic img").setAttribute("src", userAvatar);
+	userAvatar() {
+		return getGravatarFromEmail(getEmail(), 200);
 	}
 
 	render() {
@@ -20,13 +20,13 @@ export default class ProfileSidebarPTR extends Component {
 			<div className="profile-sidebar">
 				<div className="portlet light profile-sidebar-portlet ">
 					<div className="profile-userpic">
-						<img src="" className="img-responsive" alt=""/></div>
+						<img src={this.userAvatar()} className="img-responsive" alt=""/></div>
 					<div className="profile-usertitle">
 						<div className="profile-usertitle-name"> {initData}  </div>
 					</div>
 					<div className="profile-userbuttons" style={{paddingBottom: '30px'}}>
 						<button type="button" className="btn btn-circle red btn-sm logout-btn"
-								onClick={this.props.SubmitLogout.bind(this)}>خروج
+								onClick={this.props.SubmitLogout.bind(this)}>{translate('Logout')}
 						</button>
 					</div>
 				</div>
