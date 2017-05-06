f4e2977410e0bfc37c4d4296d3ccbb321c464176
hasan gilak <hasan.gilak@outlook.com>
2017-05-06 12:26:18 +0430

    refactored

--- src/app/components/UserProfile/ChangePasswordCTR.jsx
+++ src/app/components/UserProfile/ChangePasswordCTR.jsx
@@ -1,59 +1,55 @@
 import React, {Component} from "react";
 import ChangePasswordPTR from "./ChangePasswordPTR";
 import swagger from "./../../swagger/index";
-import {connect} from "react-redux";
 import {FailedBoxAlert, SuccessBoxAlert} from "../../functions/notifications";
 import {getToken} from "../../redux/helpers";
 import {ifInvalidToken} from "../../functions/helpers";
 let Ladda = require('ladda/js/ladda');
 
-@connect()
-export default class ChangePasswordCTR extends Component {
-	loadingProgress;
+let loadingProgress;
 
-	ChangePasswordCallback({error, data, response}, reset) {
-		if (response.statusCode == '200') {
-			this.loadingProgress.stop();
-			reset();
-			SuccessBoxAlert({
-				error: 'اطلاعات شما صحیح نمی‌باشد.',
-				text: 'اطلاعات شما با موفقیت ثبت شد.'
-			});
-		} else if (response.statusCode == '400') {
-			this.stopLoading();
-			FailedBoxAlert({
-				error: 'اطلاعات شما صحیح نمی‌باشد.',
-				text: 'اطلاعات شما با موفقیت ثبت شد.'
-			});
-		}
-		ifInvalidToken(response);
-	}
+function ChangePasswordCallback({response}, reset) {
+    if (response.statusCode === 200) {
+        loadingProgress.stop();
+        reset();
+        SuccessBoxAlert({
+            error: 'اطلاعات شما صحیح نمی‌باشد.',
+            text: 'اطلاعات شما با موفقیت ثبت شد.'
+        });
+    } else if (response.statusCode === 400) {
+        stopLoading();
+        FailedBoxAlert({
+            error: 'اطلاعات شما صحیح نمی‌باشد.',
+            text: 'اطلاعات شما با موفقیت ثبت شد.'
+        });
+    }
+    ifInvalidToken(response);
+}
 
-	ChangePasswordCall(formValues, reset) {
-		(new swagger.UserApi())
-			.userChangePasswordPost(getToken(), {'payloadData': formValues})
-			.then(response => this.ChangePasswordCallback(response, reset));
-	}
+function ChangePasswordCall(formValues, reset) {
+    (new swagger.UserApi())
+        .userChangePasswordPost(getToken(), {'payloadData': formValues})
+        .then(response => ChangePasswordCallback(response, reset));
+}
 
-	loading() {
-		this.loadingProgress = Ladda.create(document.querySelector('button.change-password-form'));
-		this.loadingProgress.start();
-	}
+function loading() {
+    loadingProgress = Ladda.create(document.querySelector('button.change-password-form'));
+    loadingProgress.start();
+}
 
-	stopLoading() {
-		if (this.loadingProgress)
-			this.loadingProgress.stop();
-	}
+function stopLoading() {
+    if (loadingProgress)
+        loadingProgress.stop();
+}
 
-	SubmitChangePasswordUser = (formValues, form, reset) => {
-		if (!form.valid())
-			return;
+function  SubmitChangePasswordUser(formValues, form, reset) {
+    if (!form.valid())
+        return;
 
-		this.loading();
-		this.ChangePasswordCall(formValues, reset)
-	};
+    loading();
+    ChangePasswordCall(formValues, reset)
+}
 
-	render() {
-		return (<ChangePasswordPTR SubmitChangePasswordUser={this.SubmitChangePasswordUser}/>);
-	}
+export default function ChangePasswordCTR() {
+    return (<ChangePasswordPTR SubmitChangePasswordUser={SubmitChangePasswordUser}/>);
 }
