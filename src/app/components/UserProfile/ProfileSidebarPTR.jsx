367f4b50bf07c1b7f5dbc11f300cf56b4784ce1e
hasan gilak <hasan.gilak@outlook.com>
2017-05-06 12:49:59 +0430

    refactored

--- src/app/components/UserProfile/ProfileSidebarPTR.jsx
+++ src/app/components/UserProfile/ProfileSidebarPTR.jsx
@@ -1,40 +1,40 @@
-import React, {Component, PropTypes} from "react";
+import React, {PropTypes} from "react";
 import {getGravatarFromEmail} from "../../functions/gravatar";
-import {getCorporationTitle, getEmail, getFullName} from "./../../redux/helpers";
-import {select} from "./../../functions/select";
-import {translate} from './../../functions/translate'
+import {getEmail} from "./../../redux/helpers";
+import {translate} from "./../../functions/translate";
+import {getCorporationTitle, getFullName} from "../../redux/helpers";
+import {select} from "../../functions/select";
 
-export default class ProfileSidebarPTR extends Component {
-	userAvatar() {
-		return getGravatarFromEmail(getEmail(), 200);
-	}
+function userAvatar() {
+    return getGravatarFromEmail(getEmail(), 200);
+}
 
-	render() {
-		let initData;
-		if ((select('user.personal.first_name')) != null) {
-			initData = getFullName();
-		} else {
-			initData = getCorporationTitle();
-		}
-		return (
-			<div className="profile-sidebar">
-				<div className="portlet light profile-sidebar-portlet ">
-					<div className="profile-userpic">
-						<img src={this.userAvatar()} className="img-responsive" alt=""/></div>
-					<div className="profile-usertitle">
-						<div className="profile-usertitle-name"> {initData}  </div>
-					</div>
-					<div className="profile-userbuttons" style={{paddingBottom: '30px'}}>
-						<button type="button" className="btn btn-circle red btn-sm logout-btn"
-								onClick={this.props.SubmitLogout.bind(this)}>{translate('Logout')}
-						</button>
-					</div>
-				</div>
-			</div>
-		)
-	}
+export default function ProfileSidebarPTR(props) {
+    let initData;
+    if ((select('user.personal.first_name')) !== null) {
+        initData = getFullName();
+    } else {
+        initData = getCorporationTitle();
+    }
+
+    return (
+        <div className="profile-sidebar">
+            <div className="portlet light profile-sidebar-portlet ">
+                <div className="profile-userpic">
+                    <img src={userAvatar()} className="img-responsive" alt=""/></div>
+                <div className="profile-usertitle">
+                    <div className="profile-usertitle-name"> {initData}  </div>
+                </div>
+                <div className="profile-userbuttons" style={{paddingBottom: '30px'}}>
+                    <button type="button" className="btn btn-circle red btn-sm logout-btn" onClick={props.SubmitLogout}>
+                        {translate('Logout')}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 ProfileSidebarPTR.propTypes = {
-	SubmitLogout: PropTypes.func
+    SubmitLogout: PropTypes.func
 };
